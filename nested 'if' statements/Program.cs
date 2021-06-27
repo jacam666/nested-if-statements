fe189111af12a3e50a30ec9820e3c8ba21244e49
jacam666 <ja6cam@gmail.com>
2021-06-27 18:53:12 +0100

    Add project files.

---
+++ nested 'if' statements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace nested__if__statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int points = Convert.ToInt32(Console.ReadLine());
+           
+            if (points > 80)
+            {
+                Console.WriteLine("Level completed");
+            }
+            else
+            {
+                Console.WriteLine("You lost");
+            }
+            if (points > 90)
+            {
+                Console.WriteLine("Extra gift");
+            }
+        }
+    }
+}
